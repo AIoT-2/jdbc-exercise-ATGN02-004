df9686ab2b01387aaf275d17eca16ed3162eb9ae
rudduddl <kim5472678@gmail.com>
2025-02-03 15:04:53 +0900

    student-pagination 완료

--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -15,9 +15,9 @@ public class DbUtils {
         BasicDataSource basicDataSource = new BasicDataSource();
 
         //todo#0 {ip},{database},{username},{password}
-        basicDataSource.setUrl("jdbc:mysql://{ip}:3306/{database}");
-        basicDataSource.setUsername("{username}");
-        basicDataSource.setPassword("{password}");
+        basicDataSource.setUrl("jdbc:mysql://220.67.216.14:13306/nhn_academy_204");
+        basicDataSource.setUsername("nhn_academy_204");
+        basicDataSource.setPassword("nK3dripi!");
 
         basicDataSource.setInitialSize(5);
         basicDataSource.setMaxTotal(5);
